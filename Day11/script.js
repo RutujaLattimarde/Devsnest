5210081f0aad01877d0ee9e98de71dae59119873
RutujaLattimarde <rutujalattimarde@gmail.com>
2021-07-19 15:51:57 +0530

    day 11

--- Day11/script.js
+++ Day11/script.js
@@ -70,7 +70,7 @@ function showProgress() {
  
 function showScores() {
     var gameOverHTML = "<h1>Result</h1>";
-    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    gameOverHTML += "<h2 id='score'> Your score: " + quiz.score + "</h2>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 };
